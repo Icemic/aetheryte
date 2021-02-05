96f40269f768feda6b7e1aab1abb22cb3a44ff91
Icemic <bingfeng.jw@gmail.com>
2021-02-06 01:07:50 +0800

    feat: support transfer only

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ mod router;
 use dns::DNSServer;
 use futures::future::try_join;
 use router::Router;
+use std::env;
 use std::process::exit;
 
 #[tokio::main]
 async fn main() {
     println!("Aetheryte is running.");
+
+    let is_transfer_only = env::var("TRANSFER_ONLY")
+        .map(|v| {
+            println!("TRANSFER_ONLY: {}", v);
+            true
+        })
+        .unwrap_or(false);
 
     ctrlc::set_handler(|| {
         println!("\nGoodbye.");
@@ -16,13 +24,18 @@ async fn main() {
     })
     .expect("Error setting Ctrl-C handler");
 
-    let dns_server = DNSServer::new().await;
-    let traffic_router = Router::new().await;
+    if is_transfer_only {
+        let traffic_router = Router::new().await;
+        traffic_router.start().await.unwrap();
+    } else {
+        let dns_server = DNSServer::new().await;
+        let traffic_router = Router::new().await;
 
-    match try_join(dns_server.start(), traffic_router.start()).await {
-        Ok(_) => {}
-        Err(err) => {
-            panic!(err);
+        match try_join(dns_server.start(), traffic_router.start()).await {
+            Ok(_) => {}
+            Err(err) => {
+                panic!(err);
+            }
         }
     }
 }
