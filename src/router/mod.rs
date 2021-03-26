14929122c0febf834ad4de992a11670b542cd836
Icemic <bingfeng.web@gmail.com>
2021-03-26 12:37:44 +0000

    feat: support ipv6 redirect

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -3,48 +3,72 @@ mod passthrough;
 mod proxy;
 mod utils;
 
-use futures::FutureExt;
+use futures::{future::try_join, FutureExt};
 pub use geoip::GeoIP;
 use passthrough::passthrough;
 use proxy::proxy;
 use std::{io::Error, process::exit};
 use tokio::net::TcpListener;
-use utils::get_original_dest;
+use utils::{get_original6_dest, get_original_dest};
 
 pub struct Router {
     geoip: GeoIP,
-    server: TcpListener,
+    server_ipv4: TcpListener,
+    server_ipv6: TcpListener,
 }
 
 impl Router {
     pub async fn new() -> Self {
         let geoip = GeoIP::new().await;
 
-        let server = match TcpListener::bind("0.0.0.0:3333").await {
+        let server_ipv4 = match TcpListener::bind("0.0.0.0:3333").await {
             Ok(server) => {
                 println!("Traffic routing service now is serving on tcp://0.0.0.0:3333");
                 server
             }
-            Err(_) => {
-                println!("error on tcp listening");
+            Err(e) => {
+                println!("error on tcp listening: {}", e);
                 exit(-1);
             }
         };
 
-        Router { geoip, server }
+        let server_ipv6 = match TcpListener::bind("[::1]:3333").await {
+            Ok(server) => {
+                println!("Traffic routing service now is serving on tcp://[::1]:3333");
+                server
+            }
+            Err(e) => {
+                println!("error on tcp listening: {}", e);
+                exit(-1);
+            }
+        };
+
+        Router {
+            geoip,
+            server_ipv4,
+            server_ipv6,
+        }
     }
 
     pub async fn start(&self) -> Result<(), Error> {
+        match try_join(self.start_for_ipv4(), self.start_for_ipv6()).await {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err),
+        }
+    }
+
+    pub async fn start_for_ipv4(&self) -> Result<(), Error> {
         loop {
-            let (socket, addr) = self.server.accept().await?;
+            let (socket, addr) = self.server_ipv4.accept().await?;
 
             let (dst_ip, dst_port) = match get_original_dest(&socket) {
                 Ok(addr) => addr,
                 Err(err) => {
-                    println!("{}", err);
+                    println!("error on getting original destination: {}", err);
                     continue;
                 }
             };
+
             let country_code = self.geoip.lookup_country_code(&dst_ip);
 
             let info_message = format!(
@@ -71,6 +95,37 @@ impl Router {
                 });
                 tokio::spawn(transfer);
             }
+        }
+        #[allow(unreachable_code)]
+        Ok(())
+    }
+
+    pub async fn start_for_ipv6(&self) -> Result<(), Error> {
+        loop {
+            let (socket, addr) = self.server_ipv6.accept().await?;
+
+            let (dst_ip, dst_port) = match get_original6_dest(&socket) {
+                Ok(addr) => addr,
+                Err(err) => {
+                    println!("error on getting original destination: {}", err);
+                    continue;
+                }
+            };
+
+            let info_message = format!(
+                "from {}, to {}:{} in Unknown(ipv6 address)",
+                addr, dst_ip, dst_port
+            );
+
+            let transfer = passthrough(socket, dst_ip, dst_port, info_message).map(|r| {
+                if let Err(e) = r {
+                    println!("Failed to transfer {}", e);
+                } else if let Ok(info) = r {
+                    println!("Success to transfer {}", info);
+                }
+            });
+
+            tokio::spawn(transfer);
         }
         #[allow(unreachable_code)]
         Ok(())
--- src/router/utils.rs
+++ src/router/utils.rs
@@ -1,5 +1,6 @@
 use nix::sys::socket::getsockopt;
 use nix::sys::socket::sockopt::OriginalDst;
+use nix::sys::socket::sockopt::Ip6tOriginalDst;
 use std::{io::ErrorKind, net::IpAddr, os::unix::io::AsRawFd};
 use tokio::io;
 use tokio::net::TcpStream;
@@ -12,16 +13,30 @@ pub fn get_original_dest(fd: &TcpStream) -> io::Result<(IpAddr, u16)> {
         nix::Error::Sys(err) => io::Error::from(err),
         _ => io::Error::new(ErrorKind::Other, e),
     })?;
-    // libc::IP6T_SO_ORIGINAL_DST
-    libc::SOL_IPV6;
+
     let ip = match addr.sin_family {
-        AF_INET => {
-            IpAddr::V4(u32::from_be(addr.sin_addr.s_addr).into())
-        }
-        AF_INET6 => {
-            IpAddr::V6(u128::from_be(addr.sin_addr.s_addr).into())
-        }
+        AF_INET => IpAddr::V4(u32::from_be(addr.sin_addr.s_addr).into()),
+        _ => return Err(io::Error::new(ErrorKind::Other, "Unrecognized ip family.")),
     };
     let port = u16::from_be(addr.sin_port);
+    Ok((ip, port))
+}
+pub fn get_original6_dest(fd: &TcpStream) -> io::Result<(IpAddr, u16)> {
+    let addr = getsockopt(fd.as_raw_fd(), Ip6tOriginalDst).map_err(|e| match e {
+        nix::Error::Sys(err) => io::Error::from(err),
+        _ => io::Error::new(ErrorKind::Other, e),
+    })?;
+    // libc::IP6T_SO_ORIGINAL_DST
+    // libc::SOL_IPV6;
+    let ip = match addr.sin6_family {
+        AF_INET6 => {
+            let addr: *const libc::sockaddr_in6 =
+                (&addr) as *const _ as *const libc::c_void as *const libc::sockaddr_in6;
+            let addr = unsafe { *addr };
+            IpAddr::V6(addr.sin6_addr.s6_addr.into())
+        }
+        _ => return Err(io::Error::new(ErrorKind::Other, "Unrecognized ip family.")),
+    };
+    let port = u16::from_be(addr.sin6_port);
     Ok((ip, port))
 }
